e28dac1b77e62a0f970efa901cbf2d3c2f7d9a4b
Ste Prescott <steprescott@users.noreply.github.com>
2025-01-28 12:06:13 +0000

    feat!: v3 SDK
    BREAKING CHANGE: SDK v3 release. See CHANGELOG.md for details.

--- CobrowseIO.xcframework/ios-arm64/CobrowseIO.framework/Headers/CBIOHasViewProperties.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-@protocol CBIOHasViewProperties <NSObject>
-
-@property (copy, nullable) NSString *tag;
-@property (copy, nullable) NSString *id;
-@property (copy, nonnull) NSDictionary<NSString *, NSString *> *attributes;
-
-@end
--- CobrowseIOAppExtension.xcframework/ios-arm64_x86_64-simulator/CobrowseIOAppExtension.framework/Headers/CobrowseIOReplayKitExtension.h
+++
@@ -1,6 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <ReplayKit/ReplayKit.h>
-
-@interface CobrowseIOReplayKitExtension : RPBroadcastSampleHandler
-
-@end
