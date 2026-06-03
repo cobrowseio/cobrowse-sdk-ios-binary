f317ec6dbb1628889e542b070fc6b86b32fcd3de
Craig Thomas <craig006@gmail.com>
2026-06-03 05:17:01 +0100

    chore: Update to iOS 15

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
     name: "CobrowseSDK",
     platforms: [
-        .macOS(.v10_13), .iOS(.v12)
+        .macOS(.v10_13), .iOS(.v15)
     ],
     products: [
         .library(
